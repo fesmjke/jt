dccd6d15013d915cd56d472506988e249952c7de
Fatal Error <fatalerror.sm@gmail.com>
2019-01-10 04:03:02 +0200

    changed some requests

--- application/app.js
+++ application/app.js
@@ -3,51 +3,38 @@ const pug = require("pug");
 const config = require("../config");
 const bodyParser = require("body-parser");
 const User = require("../models/user");
-const path = require("path")
-
-
+const path = require("path");
 
 const app = express();
 
 app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get("/index", (req, res) => {
-  res.render("index.pug");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/users',(req,res) => {
-    User.find({}).then(users => {
-        res.render('users',{users : users})
-    });
+app.get("/users", (req, res) => {
+  User.find({}).then(users => {
+    res.render("users", { users: users });
+  });
 });
 
-app.get('/dev/n',(req,res) => {
-  res.render('layout/navigation');
+app.get("/dev/n", (req, res) => {
+  res.render("layout/navigation");
 });
 
-app.get('/dev/c',(req,res) =>{
-  res.render('layout/carousel');
+app.get("/dev/c", (req, res) => {
+  res.render("layout/carousel");
 });
 
-app.get('/dev/l',(req,res) =>{
-  res.render('singIN');
+app.get("/dev/l", (req, res) => {
+  res.render("singIN");
 });
 
-app.get('/dev/r',(req,res) =>{
-  res.render('singUP');
-});
-
-app.post("/index", (req, res) => {
-  const { firstName, lastName } = req.body;
-
-  User.create({
-    firstName,
-    lastName
-  }).then(user => console.log(user));
-
-  res.redirect("index");
+app.get("/dev/r", (req, res) => {
+  res.render("singUP");
 });
 
 module.exports = app;
