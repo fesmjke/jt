20f5cbb7981a310fd1388609f4c6a622ca23357f
Fatal Error <fatalerror.sm@gmail.com>
2019-01-12 00:01:07 +0200

    changes in model structure

--- models/user.js
+++ models/user.js
@@ -1,15 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const user = new Schema({
-    firstName:{
-        type:String,
-        required:true
-    },
-    lastName:{
-        type:String,
-        required:true
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    sparse:true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('User',user);
+user.set('toJSON',{
+    virtuals:true
+})
+
+module.exports = mongoose.model("User", user);
