9b28cc4e0475978a8eb53c25c024fae3b2c7eb0e
Fatal Error <fatalerror.sm@gmail.com>
2019-01-15 04:17:02 +0200

    adding new router - for user profile

---
+++ routes/user.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const models = require("../models");
+
+router.get("/", (req, res) => {
+  const userId = req.session.userId;
+  const userEmail = req.session.userLogin;
+  let userToSend = null;
+  let postsToSend = null;
+  if (!userId || !userEmail) {
+    res.redirect("/");
+  } else {
+    models.User.findById(userId)
+      .then(user => {
+        if (user) {
+          userToSend = user;
+          models.Post.find({}).then(posts => {
+            postsToSend = posts;
+            res.render("user/profile", {
+              p:postsToSend,
+              userToSend,
+              user: {
+                nk: user.nickname,
+                id: userId
+              }
+            });
+          });
+          
+        } else {
+          res.redirect("/");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect("/");
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect("/");
+      });
+  }
+});
+
+module.exports = router;
