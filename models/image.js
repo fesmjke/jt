d0c9ad87b17b7374bc9278a17899dab068149aaf
Fatal Error <fatalerror.sm@gmail.com>
2019-01-16 04:10:09 +0200

    new model - image for load images to database

---
+++ models/image.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const image = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    },
+    path: {
+      type: String,
+      required: true
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+image.set("toJSON", {
+  virtuals: true
+});
+
+module.exports = mongoose.model("Image", image);
